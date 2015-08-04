0dba3849c968f258cf0b7ca2a7b0fc0b65282f42
longleggedone <longlegsclassic@gmail.com>
2015-08-04 15:43:43 -0400

    procGenPrototype

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	
+	
+
+	public BoardManager boardScript;
+
+	// Use this for initialization
+	void Awake () {
+		boardScript = GetComponent<BoardManager>();
+		StartGame();
+	}
+	
+	void StartGame(){
+		boardScript.SetupScene();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/scripts/Reload.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Reload : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.R)){
+			Application.LoadLevel (Application.loadedLevel);
+		}
+	}
+}
