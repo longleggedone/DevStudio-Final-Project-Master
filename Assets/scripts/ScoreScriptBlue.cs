aaf937b2e91b2cd568743e9b0834f204d24d76d4
Pedro Mont <pm2558@nyu.com>
2015-08-10 15:44:33 -0400

    Scoring System done!

---
+++ Assets/scripts/ScoreScriptBlue.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreScriptBlue : MonoBehaviour {
+
+	public Text BlueScoreText;
+	public int ScoreBlue = 0;
+	
+	void OnTriggerStay ( Collider activator ) {
+		
+		if (Input.GetKeyDown (KeyCode.Q)) { 
+			ScoreBlue++;
+			Destroy ( activator.gameObject );
+			
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		BlueScoreText.text = "Blue [Q] \nScore: " + ScoreBlue.ToString();
+	}
+
+}
