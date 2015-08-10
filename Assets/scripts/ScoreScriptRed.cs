30415b9f7c8ec570e612e44898dd17c46e940258
Pedro Mont <pm2558@nyu.com>
2015-08-10 14:37:36 -0400

    Score Script done (only for Red Player)

---
+++ Assets/scripts/ScoreScriptRed.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreScriptRed : MonoBehaviour {
+
+
+	public int ScoreRed = 0;
+
+	void OnTriggerStay ( Collider activator ) {
+
+		if (Input.GetKeyDown (KeyCode.R)) { 
+			transform.Rotate ( 0f, 5f, 0f ); // if triggered, then spin
+			ScoreRed++;
+			Debug.Log (ScoreRed);
+			Destroy ( activator.gameObject );
+
+		}
+	}
+
+}
