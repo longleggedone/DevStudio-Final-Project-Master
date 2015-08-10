ebd11bd00ac4caf80b7934d8d826a51d39f82e5c
Danny Kim <daniel.c.kim@gmail.com>
2015-08-09 21:40:33 -0400

    ballSpawner w/o moneyball

---
+++ Assets/scripts/ballSpawner.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballSpawner : MonoBehaviour {
+
+	public GameObject[] balls;
+	public int volleyCount = 5;
+	public int numberOfBalls = 20;
+	public GameObject moneyBall;
+	
+	
+	void Start () {
+		InvokeRepeating("volleySpawn", 2f, 15f ); // 5 balls are created every 15 seconds
+		
+		StartCoroutine(moneyBallSpawn());
+	}
+	
+	
+	void volleySpawn () {
+		for ( int i = 0; i < volleyCount; i++) {
+			Invoke( "ballSpawn", 0.5f * i);
+			//volleyCount=5;
+		}
+	}
+
+	
+	void ballSpawn () {
+		
+		GameObject randomBall = balls[Random.Range (0, balls.Length)];
+		Instantiate ( randomBall, gameObject.transform.position, Quaternion.identity );
+		
+	}
+
+	IEnumerator moneyBallSpawn () {
+
+		yield return new WaitForSeconds (Random.Range (1, 10));
+		Instantiate (moneyBall, gameObject.transform.position, Quaternion.identity);
+	
+	}
+	
+	
+	
+	void Update () {
+		
+		
+		// Destroys gameObject "Spawner" after certain amount of balls are created 
+		GameObject[] GameBall = GameObject.FindGameObjectsWithTag ("GameBall");
+		
+		int ballCounter = GameBall.Length;
+		
+		// set to destroy gameObject "Spawner after 20 balls are creared.
+		// change number to decide how many balls will be produced.
+		if ( ballCounter == numberOfBalls ) { 
+			Destroy (this.gameObject);
+		}
+		
+	}
+	
+}
