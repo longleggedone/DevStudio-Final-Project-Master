6e514e4fab39538e3db725b2d58aa1f79a5a1cca
Pedro Mont <pm2558@nyu.com>
2015-08-11 16:31:30 -0400

    EndGame in progress

---
+++ Assets/scripts/EndGame.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EndGame : MonoBehaviour {
+
+	public ScoreScriptBlue ScoreB; // assign in inspector
+	public ScoreScriptRed ScoreR; // assign in inspector
+	public ScoreScriptGreen ScoreG; // assign in inspector
+	public ScoreScriptYellow ScoreY; // assign in inspector
+	public int ObjectiveScore = 30;
+
+	public Text VictoryText; // assign in inspector
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		if (ScoreB.ScoreBlue > ObjectiveScore) { 
+			VictoryText.text = "Player Blue WINS!!!";
+		} else if (ScoreG.ScoreGreen > ObjectiveScore) {
+			VictoryText.text = "Player Green WINS!!!";
+		} else if (ScoreY.ScoreYellow > ObjectiveScore) {
+			VictoryText.text = "Player Yellow WINS!!!";
+		} else if (ScoreR.ScoreRed > ObjectiveScore) {
+			VictoryText.text = "Player Red WINS!!!";
+		}
+	
+	}
+}
